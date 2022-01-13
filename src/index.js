38fe72646e33cfbcf9670b4288fe18b4bd23958b
Jasleen <jasleen0859@gmail.com>
2022-01-13 17:23:04 -0600

    Displayed a hello on the screen usin react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Step 1 - Import the react and react dom libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
+// Step 2 - Create a react component -
+const App = () =>  {
+  return <div> Hi there! </div>;
+};
+
+const App2 = function() {
+  return <div> Hi there! </div>;
+}
+
+// Step - 3 Take the react component and show it on the screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />,
+  document.querySelector('#root') // using css selector to reference the html element - could also use getElementByID as well I guess
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
